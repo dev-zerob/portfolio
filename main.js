24dcd73ec2116ced80494a6a82ad03e2cb01dc21
Jaden <dev.zerob@gmail.com>
2022-03-22 13:11:05 +0900

    Add Project Filtering

--- main.js
+++ main.js
@@ -51,6 +51,30 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (event) => {
+  const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add('animate-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === 'all' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    
+    projectContainer.classList.remove('animate-out');
+  }, 300);
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth"});
